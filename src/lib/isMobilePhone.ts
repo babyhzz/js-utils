9257ac9c0eb95e1c6389e7bdcc709230900d1356
hzz <383686357@qq.com>
2022-03-04 21:34:58 +0800

    0.0.3

--- src/lib/isMobilePhone.ts
+++ src/lib/isMobilePhone.ts
@@ -1,5 +1,10 @@
 const reg = /^((\+|00)86)?(1[3-9]|9[28])\d{9}$/;
 
+/**
+ * 判断字符串是否是一个手机号
+ * @param phone 手机号字符串
+ * @returns true or false
+ */
 function isMobilePhone(phone: string): boolean {
   return reg.test(phone);
 }
